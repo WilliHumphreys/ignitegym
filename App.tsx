53bab3afe452ab26e5b55d6696646b22e189f460
Willi Humphreys <willighn469@gmail.com>
2025-05-17 11:57:56 -0300

    Imagem de Background

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
 
 import { config } from "./config/gluestack-ui.config";
 import { Loading } from "@components/Loading";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -19,9 +20,7 @@ export default function App() {
         translucent
       />
       {fontsLoaded ? (
-        <Center flex={1} bg="$gray700">
-          <Text>Home</Text>
-        </Center>
+        <SignIn />
       ) : (
         <Loading />
       )}
