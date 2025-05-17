3d3f344721bc232623a8ea1fdaa9ee26ac9738bd
Willi Humphreys <willighn469@gmail.com>
2025-05-17 11:43:22 -0300

    Componentizando o tema padrão

--- App.tsx
+++ App.tsx
@@ -4,27 +4,26 @@ import {
   Roboto_700Bold,
   Roboto_400Regular,
 } from "@expo-google-fonts/roboto";
-import { GluestackUIProvider, Text } from "@gluestack-ui/themed"
+import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
+
+import { config } from "./config/gluestack-ui.config";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
   return (
-    <GluestackUIProvider>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "#202024",
-        }}
-      >
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        {fontsLoaded ? <Text color="white">Home</Text> : <View />}
-      </View>
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Center flex={1} bg="$gray700">
+          <Text>Home</Text>
+        </Center>
+      ) : (
+        <View />
+      )}
     </GluestackUIProvider>
   );
 }
